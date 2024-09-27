ace07dfb7dd0629763f75dc78035bd46b6eb4e75
mattvie <mvmf@cin.ufpe.br>
2024-09-27 17:07:33 -0300

    testing pathing on react

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import { Routes, Route } from 'react-router-dom';
 import Layout from './Pages/Layout';
 import FlexPage from './Pages/Flex/FlexPage'
 import FlexExamples from './Pages/Flex/FlexExamples'
+import FlexTest1 from './Pages/FlexTest1'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route path="/flexbox-simulator" element={<Layout />}>
         <Route index element={<FlexPage />} />
+        <Route path={'/'} element={<FlexTest1 />} />
+        <Route path={'/simulator'} element={<FlexPage />} />
         <Route path={'/examples'} element={<FlexExamples />} />
-        <Route path={'/flexbox-simulator'} element={<FlexPage />} />
       </Route>
     </Routes>
   );
---
+++ src/Pages/FlexTest1.js
@@ -0,0 +1,7 @@
+export default function FlexTest1() {
+    return (
+        <body>
+            test 1
+        </body>
+    );
+}
