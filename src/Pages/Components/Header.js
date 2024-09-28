008486c6f7eccddc92449c56d995044ed8fdfabb
mattvie <mvmf@cin.ufpe.br>
2024-09-27 21:04:37 -0300

    hash router

--- src/Pages/Components/Header.js
+++ src/Pages/Components/Header.js
@@ -1,13 +1,19 @@
 import './Header.css'
 
 export default function Header() {
+    const currentPath = window.location.pathname;
+
     return (
         <header className="header">
-            <a href="/flexbox-simulator" className="index">Flexbox</a>
+            <a href="/#/flexbox-simulator" className="index">Flexbox Simulator</a>
 
             <nav>
-                <a href="/flexbox-simulator">Simulator</a>
-                <a href="/flexbox-simulator/examples">Examples</a>
+                {currentPath !== '/flexbox-simulator' && (
+                    <a href="/#/flexbox-simulator">Simulator</a>
+                )}
+                {currentPath !== '/flexbox-simulator/examples' && (
+                    <a href="/#/flexbox-simulator/examples">Basic Examples</a>
+                )}
             </nav>
         </header>
     );
